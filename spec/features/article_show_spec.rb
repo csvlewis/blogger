c54cd1f69fbb23af9f0dd11c34417bade6e70463
Chris Lewis <13chlewis@gmail.com>
2019-02-07 06:52:02 -0700

    Added functionality to delete articles

--- spec/features/article_show_spec.rb
+++ spec/features/article_show_spec.rb
@@ -12,5 +12,19 @@ describe "article show page" do
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
     end
+
+    describe "and they click Delete Article" do
+      it "displays all articles without the deleted entry" do
+        article_1 = Article.create!(title: "Title 1", body: "Body 1")
+        article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+        visit article_path(article_1)
+        click_link "Delete"
+
+        expect(current_path).to eq(articles_path)
+        expect(page).to have_content(article_2.title)
+        expect(page).to_not have_content(article_1.title)
+      end
+    end
   end
 end
